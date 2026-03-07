febbc5d459464c2d949684a5fcd62191eeaff2a2
MinaAlmasi <mina@cc.au.dk>
2026-03-07 22:20:45 +0100

    working version without dict

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function GuessInput() {
-    const [guess, setGuess] = React.useState([])
+    const [guess, setGuess] = React.useState('')
 
     return (
       <>
@@ -9,12 +9,10 @@ function GuessInput() {
       className="guess-input-wrapper"
       onSubmit={(event) => {
         event.preventDefault();
-        
-        // fix onchange!!!
-        newGuess = {"guess": event.target.value}
-
+        const newGuess = event.target.value
         setGuess(newGuess)
-        console.log(newGuess)
+        console.log(guess)
+
         setGuess('')
       }} 
       >
@@ -22,11 +20,13 @@ function GuessInput() {
           Enter guess:
         </label>
         <input 
-          id='guess-input' 
-          type="text" 
+          id='guess-input'
+          type="text"
           value={guess} 
-          onChange={event => {setGuess(event.target.value.toUpperCase())}}
-          pattern="[A-Za-z]{5,5}"
+          onChange={event => {
+          setGuess(event.target.value.toUpperCase());
+          }}
+          pattern="[A-Z]{5,5}"
           title="Please provide exactly 5 letters"
         >
         </input>
