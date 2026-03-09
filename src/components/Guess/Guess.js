d439631d9ce34507b02dfd6f5c126f651c0f490c
MinaAlmasi <mina@cc.au.dk>
2026-03-09 10:56:11 +0100

    add  suffix function to do conditional rendering

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
-import {range} from "../../utils";
+import { range } from "../../utils";
+import { answer } from "../Game";
+import { checkGuess } from "../../game-helpers"
 
 function Guess({ value }) {
+  const checkedGuess = checkGuess(value, answer)
+
+  function getClassSuffix(num) {
+    if (checkedGuess !=  null) {
+      const suffix = checkedGuess[num].status
+      return suffix
+    } else {
+      return ''
+    }
+
+  }
+
   return (
     <p className="guess">
-      {range(5).map((num) => (
-        <span key={num} className="cell">
+      {range(5).map((num) => {
+        suffix = getClassSuffix(num)
+        return (
+        <span key={num} className={`cell ${suffix}`}>
           {value ? value[num] : undefined}
         </span>
-      ))}
+        );
+      })}
     </p>
   );
 }
