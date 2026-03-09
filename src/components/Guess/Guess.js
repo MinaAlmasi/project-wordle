1039ad25bd9770d65dae60946cd3ec28b2e35078
MinaAlmasi <mina@cc.au.dk>
2026-03-09 11:12:46 +0100

    refactor: do cell component instead of suffix checking

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -4,29 +4,25 @@ import { range } from "../../utils";
 import { answer } from "../Game";
 import { checkGuess } from "../../game-helpers"
 
+
+function Cell({ letter, status }) {
+  const className = status ? `cell ${status}` : 'cell';
+
+    return <span className={className}>{letter}</span>;
+}
+
 function Guess({ value }) {
   const checkedGuess = checkGuess(value, answer)
 
-  function getClassSuffix(num) {
-    if (checkedGuess !=  null) {
-      const suffix = checkedGuess[num].status
-      return suffix
-    } else {
-      return ''
-    }
-
-  }
-
   return (
     <p className="guess">
-      {range(5).map((num) => {
-        suffix = getClassSuffix(num)
-        return (
-        <span key={num} className={`cell ${suffix}`}>
-          {value ? value[num] : undefined}
-        </span>
-        );
-      })}
+      {range(5).map((num) => (
+        <Cell 
+          key = {num}
+          letter = {checkedGuess ? checkedGuess[num].letter : undefined}
+          status = {checkedGuess ? checkedGuess[num].status : undefined} 
+        />
+      ))}
     </p>
   );
 }
